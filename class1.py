74cfed1588da75cd9524ce5229aa886d28e9077c
rehan123coder <rehankhann@gmail.com>
2025-05-31 12:47:58 +0500

    class

---
+++ class1.py
@@ -0,0 +1,13 @@
+class car :
+    def __init__(self,name,colour):
+        self.name=name
+        self.colour=colour
+    def intro (self) :
+        print("this is my car",self.name)
+        print("this is my car",self.colour)
+konesegge=car("konesegge","carbenfiber")
+konesegge.intro()
+
+        
+
+       
