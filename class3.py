eb341bbb33b572efd0e0b9135166435ecbe47966
rehan123coder <rehankhann@gmail.com>
2025-06-15 15:28:20 +0500

    class

---
+++ class3.py
@@ -0,0 +1,13 @@
+class employee:
+    def __init__(self):
+        print('employee created') 
+    def __del__(self):
+        print("Destructor called")
+    def create_obj():
+        print("marking object")
+        
+        print('functon end')
+        return 
+    print('calling create_obj() function')
+    obj = create_obj()
+    print("program end")
